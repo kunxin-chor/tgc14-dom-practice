b6afaea1feeccdaee26dd6a9ab1d6ce5760f323e
kunxin-chor <chorkunxin@yahoo.com>
2021-08-26 06:55:02 +0000

    Updated questions

--- q2_more_select_practice/script.js
+++ q2_more_select_practice/script.js
@@ -4,17 +4,17 @@
 // DO NOT CHANGE THE VARIABLE NAMES
 
 // Q1 - select the first <h2> element in the document
-
+let header2 = null;
 // Q2 - select the first element with the class "highlight"
-
+let highlight = null;
 // Q3 - select the first <span class="highlight"> in the document
-
+let spanHighlight = null;
 // Q4 - select the first element that has the class 'finished'
-
+let finished = null;
 // Q5 - select the first <p> element in the document
-
+let firstParagraph = null;
 // Q6 - select the first <p class="emphasis"> in the document
-
+let emphasisParagraph = null;
 
 // LEAVE THE BELOW ALONE! DON'T TOUCH
 try {
